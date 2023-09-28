026931238cd3d23551b03dd9bd13ca9b07db8aee
Willy <willykipchumba22@gmail.com>
2023-09-28 23:10:10 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainNode = document.getElementById('main');
+mainNode.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = 'YOUR-NAME is the champion';
